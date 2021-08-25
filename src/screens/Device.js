37a6f042d904f7be58af6ebe92ea694b673015b7
Mahmudul Alam <expelmahmud@gmail.com>
2021-08-26 01:56:49 +0600

    add: Connect & Disconnect

--- src/screens/Device.js
+++ src/screens/Device.js
@@ -1,13 +1,83 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState, useCallback, useEffect} from 'react';
+import {Text, View, ScrollView, Button, StyleSheet} from 'react-native';
 
-const Device = ({navigation}) => {
-  console.log(navigation);
+const Device = ({navigation, route}) => {
+  // get the device object which was given through navigation params
+  const {device} = route.params;
+
+  const [isConnected, setIsConnected] = useState(false);
+  const [services, setServices] = useState([]);
+
+  // handle the device disconnection
+  const disconnectDevice = useCallback(async () => {
+    navigation.navigate('Home');
+    const isDeviceConnected = await device.isConnected();
+    if (isDeviceConnected) {
+      await device.cancelConnection();
+    }
+  }, [device, navigation]);
+
+  useEffect(() => {
+    const getDeviceInformations = async () => {
+      // connect to the device
+      const connectedDevice = await device.connect();
+      setIsConnected(true);
+
+      // discover all device services and characteristics
+      const allServicesAndCharacteristics =
+        await connectedDevice.discoverAllServicesAndCharacteristics();
+      // get the services only
+      const discoveredServices = await allServicesAndCharacteristics.services();
+      setServices(discoveredServices);
+    };
+
+    getDeviceInformations();
+
+    device.onDisconnected(() => {
+      navigation.navigate('Home');
+    });
+
+    // give a callback to the useEffect to disconnect the device when we will leave the device screen
+    return () => {
+      disconnectDevice();
+    };
+  }, [device, disconnectDevice, navigation]);
+
   return (
-    <View>
-      <Text>Devices Screen</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Button title="disconnect" onPress={disconnectDevice} />
+      <View>
+        <View style={styles.header}>
+          <Text>{`Id : ${device.id}`}</Text>
+          <Text>{`Name : ${device.name}`}</Text>
+          <Text>{`Is connected : ${isConnected}`}</Text>
+          <Text>{`RSSI : ${device.rssi}`}</Text>
+          <Text>{`Manufacturer : ${device.manufacturerData}`}</Text>
+          <Text>{`ServiceData : ${device.serviceData}`}</Text>
+          <Text>{`UUIDS : ${device.serviceUUIDs}`}</Text>
+        </View>
+        {/* Display a list of all services */}
+        {/* {services && services.map(service => <ServiceCard service={service} />)} */}
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+  },
+
+  header: {
+    marginBottom: 12,
+    borderRadius: 16,
+    backgroundColor: '#f1f1f1',
+    shadowColor: 'rgba(60,64,67,0.3)',
+    shadowOpacity: 0.4,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: 12,
+  },
+});
 
 export default Device;
